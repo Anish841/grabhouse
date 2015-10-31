4f97b15aba670b195abf6ca853d299da8b0af5dc
Navin Pai <lifeofnavin@gmail.com>
2015-10-31 21:36:18 +0530

    updated chrome extension

--- Chrome_Extension/GrabhouseRRR/content.js
+++ Chrome_Extension/GrabhouseRRR/content.js
@@ -1,20 +1,77 @@
 
-function callJavascriptFunction(){
-	alert("this is call")
+function callJavascriptFunction(list, routeIndices){
+    console.log(list.length)
+    console.log(routeIndices.length)
 }
 
-alert("hello");
-var continueExecution = function(){
-var cusid_ele = document.getElementsByClassName('action-block');
- //alert(cusid_ele.length)
-for (var i = 0; i < cusid_ele.length; ++i) {
-      var item = cusid_ele[i];  
-		var button = document.createElement("button");
-		button.onclick = callJavascriptFunction;
-    var text = document.createTextNode("select for route planner");
-    button.appendChild(text);
-    item.appendChild(button);
+var routeIndices=[]
+
+function toggleArrayItem(v) {
+    var i = routeIndices.indexOf(v);
+    if (i === -1)
+        routeIndices.push(v);
+    else
+        routeIndices.splice(i,1);
 }
-};
-setTimeout(continueExecution, 10000)
+
+setTimeout(function(){
+    var passToJS = function(){
+        callJavascriptFunction(houselist);
+    }
+
+    var toggleAdd = function(){
+        index=$($(this).parentsUntil("div .list-block-inner")[5]).index();
+        toggleArrayItem(index);
+        console.log(routeIndices)
+        console.log('route updated')
+        event.preventDefault();
+        event.stopPropagation();
+        event.stopImmediatePropagation(); 
+    }
+
+    var cusid_ele = document.getElementsByClassName('action-block');
+    //alert(cusid_ele.length)
+    for (var i = 0; i < cusid_ele.length; ++i) {
+      var item = cusid_ele[i];
+	  var newdiv = document.createElement("div");
+	  newdiv.className='like-btn';
+      var button=document.createElement("i");
+      button.className='fa fa-taxi';
+      button.onclick = toggleAdd;
+      //button.onclick = passToJS;
+      newdiv.appendChild(button);
+      // var text = document.createTextNode("select for route planner");
+      // button.appendChild(text);
+      item.appendChild(newdiv);
+      item.style.display = "inline-flex";
+    }
+    var cusid_ele = document.getElementsByClassName('action-block');
+
+    var s = document.createElement('script');
+    s.src = chrome.extension.getURL('angular_inject.js');
+    (document.head||document.documentElement).appendChild(s);
+    s.onload = function() {
+        s.parentNode.removeChild(s);
+    };
+   
+
+    var houselist;
+
+    // Event listener
+    document.addEventListener('RW759_connectExtension', function(e) {
+        // e.detail contains the transferred data (can be anything, ranging
+        // from JavaScript objects to strings).
+        // Do something, for example:
+        for(var i=0;i<e.detail.listings.length;i++){
+        
+        }
+    
+        houselist=e.detail.listings
+        console.log(e.detail.listings.length);
+    });
+
+    //var listings = window.angular.element('[ng-controller=listBlkCtrl]').scope().listings
+    //alert(listings.len)
+
+},  10000)
 
