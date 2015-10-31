a815cc311277f06354d57ceadbf87603eb7c8893
Anish Ratnawat <anish.ratnawat@gmail.com>
2015-10-31 18:20:02 +0530

    Chrome Extension

---
+++ Chrome_Extension/DUmmy/content.js
@@ -0,0 +1,19 @@
+
+function callJavascriptFunction(){
+	alert("this is call")
+}
+
+var continueExecution = function(){
+var cusid_ele = document.getElementsByClassName('action-block');
+ //alert(cusid_ele.length)
+for (var i = 0; i < cusid_ele.length; ++i) {
+      var item = cusid_ele[i];  
+		var button = document.createElement("button");
+		button.onclick = callJavascriptFunction;
+    var text = document.createTextNode("select for route planner");
+    button.appendChild(text);
+    item.appendChild(button);
+}
+};
+setTimeout(continueExecution, 10000)
+
