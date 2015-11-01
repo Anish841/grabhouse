7f4ea824e2b975e7296b3e92ba77299bc306a438
Navin Pai <lifeofnavin@gmail.com>
2015-11-01 09:28:45 +0530

    added webapp

--- webapp/hello.py
+++ webapp/hello.py
@@ -1,8 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/test')
 def hello():
     return '0 1 2 3'
+
+@app.route('/')
+def firstpage():
+    return render_template('ConstraintsForm.html')
+
+@app.route('/about')
+def about():
+    return '{"Team": {"name":"IIIT-B BHKs", "members": ["Anish", "Pavan", "Navin", "Ganesh", "Android Guru aka Puneeth"]}}'
+
+@app.route('/getroute')
+def getseq():
+    return render_template('ConstraintsForm.html')
+
+#if __name__ == '__main__':
+#    app.run(debug=True)
